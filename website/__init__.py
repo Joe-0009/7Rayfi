e72b43cd2227b75e848edddab56af4436b37282a
rachidi <96305570+Joe-0009@users.noreply.github.com>
2024-06-11 22:37:46 +0000

    building nav bar

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,14 @@
 from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "1337"
+    
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
+    
+    return app
